bb9c1d7b9a72a146b74b935f8ce099337eefe851
Stefan <84-stefancruz@users.noreply.gitlab.futo.org>
2025-02-03 22:44:02 +0000

    ci: setup pre-release

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,50 @@
 #!/bin/sh
 DOCUMENT_ROOT=/var/www/sources
 
+# Use environment variable to determine deployment type
+PRE_RELEASE=${PRE_RELEASE:-false}  # Default to false if not set
+
+# Determine deployment directory
+if [ "$PRE_RELEASE" = "true" ]; then
+    RELATIVE_PATH="pre-release/PeerTube"
+else
+    RELATIVE_PATH="PeerTube"
+fi
+
+DEPLOY_DIR="$DOCUMENT_ROOT/$RELATIVE_PATH"
+PLUGIN_URL_ROOT="https://plugins.grayjay.app/$RELATIVE_PATH"
+SOURCE_URL="$PLUGIN_URL_ROOT/PeerTubeConfig.json"
+
 # Take site offline
 echo "Taking site offline..."
 touch $DOCUMENT_ROOT/maintenance.file
 
 # Swap over the content
 echo "Deploying content..."
-mkdir -p $DOCUMENT_ROOT/PeerTube
-cp peertube.png $DOCUMENT_ROOT/PeerTube
-cp PeerTubeConfig.json $DOCUMENT_ROOT/PeerTube
-cp PeerTubeScript.js $DOCUMENT_ROOT/PeerTube
-sh sign.sh $DOCUMENT_ROOT/PeerTube/PeerTubeScript.js $DOCUMENT_ROOT/PeerTube/PeerTubeConfig.json
+mkdir -p "$DEPLOY_DIR"
+cp peertube.png "$DEPLOY_DIR"
+cp PeerTubeConfig.json "$DEPLOY_DIR"
+cp PeerTubeScript.js "$DEPLOY_DIR"
+
+# Update the sourceUrl in PeerTubeConfig.json
+echo "Updating sourceUrl in PeerTubeConfig.json..."
+jq --arg sourceUrl "$SOURCE_URL" '.sourceUrl = $sourceUrl' "$DEPLOY_DIR/PeerTubeConfig.json" > "$DEPLOY_DIR/PeerTubeConfig_temp.json"
+if [ $? -eq 0 ]; then
+    mv "$DEPLOY_DIR/PeerTubeConfig_temp.json" "$DEPLOY_DIR/PeerTubeConfig.json"
+else
+    echo "Failed to update PeerTubeConfig.json" >&2
+    exit 1
+fi
+
+sh sign.sh "$DEPLOY_DIR/PeerTubeScript.js" "$DEPLOY_DIR/PeerTubeConfig.json"
 
 # Notify Cloudflare to wipe the CDN cache
 echo "Purging Cloudflare cache for zone $CLOUDFLARE_ZONE_ID..."
 curl -X POST "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/purge_cache" \
      -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
      -H "Content-Type: application/json" \
-     --data '{"files":["https://plugins.grayjay.app/PeerTube/peertube.png", "https://plugins.grayjay.app/PeerTube/PeerTubeConfig.json", "https://plugins.grayjay.app/PeerTube/PeerTubeScript.js"]}'
+     --data '{"files":["'"$PLUGIN_URL_ROOT/peertube.png"'", "'"$PLUGIN_URL_ROOT/PeerTubeConfig.json"'", "'"$PLUGIN_URL_ROOT/PeerTubeScript.js"'"]}'
 
 # Take site back online
 echo "Bringing site back online..."
-rm $DOCUMENT_ROOT/maintenance.file
+rm "$DOCUMENT_ROOT/maintenance.file"
